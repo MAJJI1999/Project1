5fd6d434dd7bd306ccf8a242d65c370b34af0713
MAJJI1999 <61636463+MAJJI1999@users.noreply.github.com>
2020-04-27 17:59:20 +0000

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,22 @@
+#load data
+
+data<-read.csv(file="household_power_consumption.txt",header=TRUE,sep=";",quote="\"")
+
+# convert the date variable to Date class
+
+data$Date<-as.Date(data$Date,"%d/%m/%Y")
+data<-data[(data$Date>= "2007-02-01") & (data$Date <= "2007-02-02"),]
+
+# Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
+
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data$Time <- strptime(data$Time, format="%H:%M:%S")
+data[1:1440,"Time"] <- format(data[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+data[1441:2880,"Time"] <- format(data[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+png("plot3.png",width=480, height=480)
+plot(data$Time,data$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(data,lines(Time,as.numeric(as.character(Sub_metering_1))))
+with(data,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(data,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
